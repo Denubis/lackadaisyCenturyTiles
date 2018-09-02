0d1d7c3ab9b3481d143ff72a063ac90b2df01768
Brian Ballsun-Stanton <brian@fedarch.org>
2018-09-02 23:01:27 +1000

    fixed url

--- makeHtml.sh
+++ makeHtml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+reponame=$(basename `git rev-parse --show-toplevel`)
 for file in *.dzi;
         do
         filename=$(basename $file ".dzi")
         echo ${filename}.html
-        sed "s/{1}/$file/g" template.html > ${filename}.html
+        sed "s/{1}/$file/g" template.html | sed "s/{2}/$reponame/g" > ${filename}.html
         done
